d0851f717290f193138f600979bfb840ee22b844
Pedro <ispedromartins@gmail.com>
2021-11-12 17:38:03 -0300

    create the integration with de backend

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,25 +1,34 @@
 
 
 import { useState, useEffect } from 'react'
-import { getDevices } from '../../Services/api'
+import { getDeviceApiList } from '../../Services/api'
 
 export default function Dashboard() {
 
-  const [listOfDevices, setListOfDevices] = useState([{ a: 'a', b: 'b' },])
-
+  const [listOfDevices, setListOfDevices] = useState([])
 
   useEffect(() => {
-    getDevices()
+    async function getDeviceList() {
+      try {
+        const res = await getDeviceApiList()
+        if (res.status === 200) return setListOfDevices(res.data)
+
+      } catch (error) {
+
+      }
+    }
+    getDeviceList()
   }, [])
 
   return (
     <div>
-      {listOfDevices.map((device) => {
+      {listOfDevices?.length && listOfDevices.map((device) => {
         return (
-          <>
-            <div>{device.a}</div>
-            <div>{device.b}</div>
-          </>
+          <div key={device.id}>
+            <div>{device.system_name}</div>
+            <div>{device.type}</div>
+            <div>{device.hdd_capacity}</div>
+          </div>
         )
       })}
     </div>
